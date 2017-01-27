2e40582748f9fef58d5fee46b4c39fa52245e611
Florian Sittel <sittel@lettis.net>
2017-01-27 11:35:48 +0100

    corrected a comment

--- src/circacf.cpp
+++ src/circacf.cpp
@@ -11,7 +11,7 @@ using namespace Rcpp;
 //' Compute ACF of circular data
 //'
 //' Computes the autocorrelation of circular data,
-//' **defined on the range [0, 2pi], expressed in radians.**
+//' **defined on the range [-pi, pi], expressed in radians.**
 //' ACF is computed in 'cov'-type.
 //' @param x The input data.
 //' @param tau_max The max correlation lag.
