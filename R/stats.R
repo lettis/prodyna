2ac82749b5e20776b58dc5c73730424aa0b4ba9b
Katrin Baumgaertner <katrin.baumgaertner@pluto.uni-freiburg.de>
2017-10-12 13:22:06 +0200

    autocor adapted to several dataframes/files

--- R/stats.R
+++ R/stats.R
@@ -1,8 +1,7 @@
 
-
-#' Compute autocorrelations.
+#' Autocorrelation.
 #'
-#' TODO: description
+#' Correlation between a signal and itself delayed in time.
 #'
 #' @param x Character or data.frame, either a filename or a data frame
 #'  providing the data.
@@ -14,49 +13,50 @@
 #' @param circular Logical, should autocorrelation be computed for circular
 #'  variables, e.g. dihedral angles? (default: \code{FALSE})
 #' @param circ.units Character, circular units, one of \code{c("deg", "rad")}
+#' @importFrom data.table fread
+#' @importFrom NISTunits NISTdegTOradian
+#' @return data.frame where row i corresponds to the autocorrelation for a delay
+#'  of i-1 frames.
 #' @export
 stats.autocor <- function(x, lag.max=0.25, type="correlation", columns=NULL, circular=FALSE, circ.units="deg") {
   # prepare data import
-  if (is.character(x)) {
-    # interpret as filename
+  if (is.character(x)) {                    # interpret as filename
     colSelect <- function(col) {
-      data.table::fread(input=x, select=col, verbose=FALSE, showProgress=FALSE)
+      fread(input=x, select=col, verbose=F, showProgress=F)$V1
     }
     if (is.null(columns)) {
-      columns <- names(data.table::fread(x, nrows=1))
+      columns <- names(fread(x, nrows=1))
     }
-  } else {
-    # interpret as object
-    colSelect <- function(col) {
-      x$col
-    }
+  } else {                                  # interpret as data.frame
+    colSelect <- function(col) { x[[col]] }
     if (is.null(columns)) {
-      columns <- names(x)
+      columns <- 1:ncol(x)
     }
   }
   # compute the autocorrelations
   cors <- lapply(columns, function(c) {
-    xs <- colSelect(c)[[1]]
-    N <- length(xs)
-    if (lag.max < 1) {
-      tau_max <- round(lag.max*N)
-    } else {
-      tau_max <- lag.max
-    }
-    if (circular) {
-      # ensure radians
-      if (tolower(circ.units) == "deg") {
-        xs <- NISTunits::NISTdegTOradian(xs)
+      xs <- colSelect(c)
+      N  <- length(xs)
+      if (lag.max < 1) {
+        tau_max <- round(lag.max*N)
+      } else {
+        tau_max <- lag.max
       }
-      # compute ACF (cov) on circular data
-      rho <- circacf(xs, tau_max)
-    } else {
-      # compute ordinary ACF, but with parallelized code
-      rho <- fastacf(xs, tau_max)
-    }
-    # normalize -> ACF (corr)
-    rho / rho[[1]]
+      if (circular) {
+        # ensure radians
+        if (tolower(circ.units) == "deg") {
+          xs <- NISTdegTOradian(xs)
+        }
+        # compute ACF (cov) on circular data
+        rho <- circacf(xs, tau_max)
+      } else {
+        # compute ordinary ACF, but with parallelized code
+        rho <- fastacf(xs, tau_max)
+      }
+      # normalize -> ACF (corr)
+      rho/rho[[1]]
   })
 
-  cors
+  names(cors) <- columns
+  return(data.frame(cors))
 }
